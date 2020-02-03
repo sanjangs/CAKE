7eafa7a34547959c6e56627f2ece6124194d7644
Sanjan Geet Singh <sanjangs976@gmail.com>
2020-02-03 18:09:30 +0530

    Added Query: 'What is a dog?'

--- knowledge.py
+++ knowledge.py
@@ -1,3 +1,17 @@
-noise_words = []
+noise_words = [
+    "is", "am", "are",
+    "a", "an", "the"
+]
 
-knowledge_base = []
+knowledge_base = [
+    [["what", "dog"], """Kingdom: Animalia
+Phylum: Chordata
+Class: Mammalia
+Order: Carnivora
+Family: Canidae
+Subfamily: Caninae
+Tribe: Canini
+Genus: Canis
+Species: C. lupus
+Subspecies: C. l. familiaris"""]
+]
