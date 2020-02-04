6b88458cce48872751cd9906d8b18a25b958d0ca
Sanjan Geet Singh <sanjangs976@gmail.com>
2020-02-04 11:17:14 +0530

    Added two new queries. Check for them in CHANGELOG

--- knowledge.py
+++ knowledge.py
@@ -4,14 +4,7 @@ noise_words = [
 ]
 
 knowledge_base = [
-    [["what", "dog"], """Kingdom: Animalia
-Phylum: Chordata
-Class: Mammalia
-Order: Carnivora
-Family: Canidae
-Subfamily: Caninae
-Tribe: Canini
-Genus: Canis
-Species: C. lupus
-Subspecies: C. l. familiaris"""]
+    [['what', 'dog'], "Kingdom: Animalia\nPhylum: Chordata\nClass: Mammalia\nOrder: Carnivora\nFamily: Canidae\nSubfamily: Caninae\nTribe: Canini\nGenus: Canis\nSpecies: C. lupus\nSubspecies: C. l. familiaris"],
+    [['why', 'sky', 'blue'], "Sky is blue because when the white light from the sun enters the earth's atmostphere, it seperates white light into seven distinct colours of which blue scatters the most. Because of this reason, we see the sky blue."],
+    [['why', 'there', 'leap', 'year'], "We need leap years to keep our modern-day Gregorian calendar in alignment with Earth's revolutions around the Sun.\nIt takes Earth approximately 365.242189 days, or 365 days, 5 hours, 48 minutes, and 45 seconds, to circle once around the Sun. This is called a tropical year, and astronomers measure this from the March equinox.\nHowever, the Gregorian calendar has only 365 days in a year. If we didn't add a leap day on February 29 nearly every four years, we would lose almost six hours off our calendar every single year. After only 100 years, our calendar would be off by around 24 days!"]
 ]
